476e57d8f9c51459b01b28ebcb8eae558386a7ca
Brad Goodlett <goodletb@oregonstate.edu>
2017-11-17 18:13:05 -0800

    finally fixed it.

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ server.get('/', function(req, res, next) {
     res.status(200).send('test test 123');
 });
 
+server.get('/four', function(req, res, next) {
+    res.status(200).send('four.html');
+})
 server.get('*', function(req, res, next) {
     res.status(404).send('404');
 });
 
-server.listen(3000,function() {
-    console.log('listening on port 3000');
+server.listen(3000, function() {
+    console.log('server listening on port 3000');
 });
+
+console.log('test');
