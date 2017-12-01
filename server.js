ceb62e6fdd2b6b1376f04644c0643be4090ff5b0
shonkap <shadowgotmilk@gmail.com>
2017-12-01 06:31:37 -0800

    Added some chat code, doesnt work yet?

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var server = express();
 var handlebars = require('express-handlebars');
 var MongoClient = require('mongodb').MongoClient;
+var socketio = require('socket.io').listen(4000).sockets;
 var urlDb = "mongodb://localhost:27017/mydb";
+
+//var io = require('socket.io').listen(server);
+//var users = [];
+//var connections = [];
+
 
 MongoClient.connect(urlDb, function(err, db) {
   if (err) throw err;
@@ -16,7 +22,48 @@ MongoClient.connect(urlDb, function(err, db) {
 		}
 		db.close();
 	});
-}); 
+
+
+  //chat thingy hopefully
+  socketio.on('connection', function(socket){
+    let chat = db.collection('chats');
+    //sending chat status
+    sendStatus = function(s){
+      socket.emit(s);
+    }
+
+    //gets chats from collection also limit number of chats
+    chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+      if(err) throw err;
+      //emit result
+      socket.emit('output',res);
+    });
+    socket.on('input',function(data){
+      let name = data.name;
+      let message = data.message;
+      //checking checkInput
+      if(name == '' || message == ''){
+        //send Error
+        sendStatus('Please enter name or message!!')
+      }
+      else{
+        //insert
+        chat.insert({name: name, message: message}, function(){
+          socketio.emit('output', [data]);
+          sendStatus({message: 'Message sent', clear: true});
+        });
+      }
+    });
+    //handling clear
+    socket.on('clear', function(data){
+      //remove all chats from collection
+      chat.remove({},function(){
+        socket.emit('cleared');
+      });
+    });
+  });
+
+});
 
 /*MongoClient.connect(urlDb, function(err, db) {
 	if (err) throw err;
@@ -85,11 +132,45 @@ server.post("/four", function(req,res) {
 		    	db.close();
 			});
 	});
-	
+
 });
 
 });
+
+
+/*io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	//Disconnect
+	socket.on('disconnect', function(data){
+		if(!socket.username) return;
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
+
+	//Send Message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+
+	// New User
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users', users);
+	}
+
+});*/
+
 
 server.listen(3000, function() {
     console.log('server listening on port 3000');
-}); 
+});
