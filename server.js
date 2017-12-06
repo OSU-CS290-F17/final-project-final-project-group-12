d17b9b1d686af927db03a2c386c4e491a632cbd2
shonkap <shadowgotmilk@gmail.com>
2017-12-05 19:31:27 -0800

    finish

--- server.js
+++ server.js
@@ -76,11 +76,14 @@ io.on('connection', function (socket) {
 		console.log(content.name, "has forfeit the game in room:", content.room);
 		socket.to(content.room).broadcast.emit('playerForfeit', content.name);
 	})
-	  socket.on('drawrequest', function(){
-	    console.log("player wants a call a draw");
-	    socket.in(player.room).emit('chatMessage', player.name + " votes to for a Draw!");
-	    socket.broadcast.to(player.room).emit('draw')
-  	})
+  socket.on('drawrequest', function(){
+    console.log("player wants a call a draw");
+    socket.in(player.room).emit('chatMessage', {author:player.name, text:"votes for a Draw!"});
+    socket.broadcast.to(player.room).emit('draw')
+  })
+  socket.on('drawfullfillreq', function(){
+    socket.broadcast.to(player.room).emit('drawfullfill')
+  })
 	function addToken(settings) {
 		var query = {numRoom: parseInt(settings.room)};
 		MongoClient.connect(urlDb, function(err,db) {
