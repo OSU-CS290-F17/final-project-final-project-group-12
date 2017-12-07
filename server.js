1dcee92042154c048886486cdfe29475f7376f3a
shonkap <shadowgotmilk@gmail.com>
2017-12-06 18:39:46 -0800

    added win function

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var MongoClient = require('mongodb').MongoClient;
 //var socketio = require('socket.io').sockets;
 var urlDb = "mongodb://localhost:27017/mydb";
 var gameEngine = require("./game.js");
+var checkWin = require("./checkforwin.js");
 //var io = require('socket.io').listen(server);
 
 
@@ -108,6 +109,12 @@ io.on('connection', function (socket) {
 					io.in(player.room).emit('newToken', {x : settings.column, color: result[0].colors[result[0].players.indexOf(settings.player)], y: result[0].board[settings.column].length-1, turn :result[0].turn});
 					db.close();
 				}
+        if(checkWin.checkforwin(result[0].board) == 1){
+          socket.in(player.room).emit('player1');
+        }
+        if(checkWin.checkforwin(result[0]) == 2){
+          socket.in(player.room).emit('player2');
+        }
 			});
 		});
 	}
